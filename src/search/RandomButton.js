99763396ef5f7b16876ff97b5d3359482288ad37
Ramon Osenga <ramon.osenga@sns.nl>
2017-10-01 12:39:59 +0200

    Updates

--- src/search/RandomButton.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './random.css';
-
-export function RandomButton(props) {
-    const {toRandomArticle} = props;
-    return (
-        <button className="random" onClick={toRandomArticle}>Random Article</button>
-    );
-}
